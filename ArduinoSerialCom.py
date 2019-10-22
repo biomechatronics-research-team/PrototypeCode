8f9e68bdbb0d85272f5b7b2f5487d26bdac04d6f
Ernesto Sanchez <37232798+xXbuster@users.noreply.github.com>
2019-10-21 21:39:25 -0400

    Updating with the port list

--- ArduinoSerialCom.py
+++ ArduinoSerialCom.py
@@ -1,15 +1,16 @@
 import serial
 from datetime import datetime
+import serial.tools.list_ports
 
-ser = serial.Serial('/dev/cu.usbserial-DN03AUZ6', baudrate=9600)
+ports = list(serial.tools.list_ports.comports())
+for p in ports:
+    print(p)
+
+myPort = input()
+
+ser = serial.Serial(myPort, baudrate=9600)
 
 while 1:
     arduinoData = ser.readline().decode('ascii')
     dateTimeObj = datetime.now()
     print("Position is %s at %s " % (arduinoData, dateTimeObj), "\n")
-
-
-
-
-
-

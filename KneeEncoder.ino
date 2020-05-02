4939927f4b80ea1d88cb5c22f153cbb5d7a58830
Maria A. Marrero <31069055+mariamarrero13@users.noreply.github.com>
2020-05-01 21:42:09 -0400

    Modified the way it prints to serial

--- KneeEncoder.ino
+++ KneeEncoder.ino
@@ -1,5 +1,4 @@
 #include <LiquidCrystal.h> 
-int Contrast=100;
 int knee = A5;
 int reading = 0;
 float volts = 0.0;
@@ -11,15 +10,13 @@ void setup()
  } 
 void loop()
  { 
-  delay(1000);
+  delay(2);
   reading = analogRead(knee);
   volts = reading * 0.0049; // this is not really necessary
   //angle given by linear regression:  
   angle = volts* 52.8759 + 21.1903;
   //add conversion operation from volts to degrees
   if(Serial.availableForWrite()){
-    Serial.print(angle-30,1);
-    Serial.print("\n");
-    
+    Serial.println(angle-30,1);    
   }
  }
